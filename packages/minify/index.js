ea4adda29e0049da602296936f73bf04bed777fa
Jurien Meerlo <codescapade@gmail.com>
2023-11-15 22:11:24 +0000

    added small lua minify package.

---
+++ packages/minify/index.js
@@ -0,0 +1,23 @@
+const luamin = require('luamin');
+const fs = require('fs');
+const path = require('path');
+
+async function minify() {
+  const args = process.argv;
+
+  console.log(args);
+  if (args.length < 3) {
+    return;
+  }
+
+  const filePath = path.join(process.cwd(), args[2]);
+
+  const buffer = await fs.promises.readFile(filePath);
+  const content = buffer.toString();
+
+  const minified = luamin.minify(content);
+
+  await fs.promises.writeFile(filePath, minified);
+}
+
+minify();
