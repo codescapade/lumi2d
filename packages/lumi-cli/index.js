9e994a7a9369214d0b906af2a8acb289b62340b8
Jurien Meerlo <codescapade@gmail.com>
2023-12-02 19:52:43 +0000

    added dependencies into their own package.json, added syncpack to sync versions (#17)

--- packages/lumi-cli/index.js
+++ packages/lumi-cli/index.js
@@ -12,31 +12,35 @@ program
   .command('minify')
   .description('Minify a lua file')
   .argument('<string>', 'The path to the lua file')
-  .action(async (luaPath) => {
-    const filePath = path.join(process.cwd(), luaPath);
-
-    const buffer = await fs.promises.readFile(filePath);
-    const content = buffer.toString();
-
-    const minified = luamin.minify(content);
-
-    await fs.promises.writeFile(filePath, minified);
-  });
+  .action(minify);
 
 program
   .command('pack')
   .description('Create a .love file from a folder.')
   .argument('<string>', 'The source folder')
   .argument('<string>', 'The output .love path')
-  .action(async (source, destination) => {
-    const outPath = path.join(process.cwd(), destination);
-    const output = fs.createWriteStream(outPath);
-
-    const archive = archiver('zip');
-    archive.pipe(output);
-
-    archive.directory(source, false);
-    archive.finalize();
-  });
+  .action(pack);
 
 program.parse();
+
+async function minify(luaPath) {
+  const filePath = path.join(process.cwd(), luaPath);
+
+  const buffer = await fs.promises.readFile(filePath);
+  const content = buffer.toString();
+
+  const minified = luamin.minify(content);
+
+  await fs.promises.writeFile(filePath, minified);
+}
+
+async function pack(source, destination) {
+  const outPath = path.join(process.cwd(), destination);
+  const output = fs.createWriteStream(outPath);
+
+  const archive = archiver('zip');
+  archive.pipe(output);
+
+  archive.directory(source, false);
+  archive.finalize();
+}
